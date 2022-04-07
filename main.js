9edd72105b4498503c1982d1a0770c33947e6162
hjoyheath <hillaryjoyheath@gmail.com>
2022-04-07 18:20:41 -0400

    album cover, preview link

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
-fetch(
-  "https://itunes.apple.com/search?term=lateralus&entity=musicTrack&limit=16",
-  {
-    method: "GET",
-    headers: {},
-  }
-).then(function (response) {
-  console.log(response);
-  return response.json();
-});
+let siteDiv = document.querySelector("#album");
+
+fetch("https://itunes.apple.com/search?term=TOOL&media=music", {
+  method: "GET",
+  headers: {},
+})
+  .then(function (response) {
+    // console.log(response);
+    return response.json();
+  })
+  .then(function (data) {
+    // console.log(data);
+
+    for (let i of data.results) {
+      console.log(i);
+      let artistDiv = document.createElement("p");
+      artistDiv.classList.add("artist");
+      artistDiv.innerText = i.artistName;
+      siteDiv.appendChild(artistDiv);
+      console.log(i.artistName);
+
+      let albumDiv = document.createElement("div");
+      albumDiv.innerText = i.collectionName;
+      siteDiv.appendChild(albumDiv);
+
+      let trackDiv = document.createElement("div");
+      siteDiv.appendChild(trackDiv);
+
+      let previewDiv = document.createElement("a");
+      previewDiv.href = `${i.trackViewUrl}`;
+      previewDiv.innerText = i.trackName;
+      siteDiv.appendChild(previewDiv);
+
+      let imageEl = document.createElement("img");
+      imageEl.src = i.artworkUrl100;
+      siteDiv.appendChild(imageEl);
+    }
+  });
