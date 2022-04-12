043b4b575c501e99ef317f64c523c6b2d713f92a
hjoyheath <hillaryjoyheath@gmail.com>
2022-04-11 20:44:36 -0400

    audio file, search bar updates

--- main.js
+++ main.js
@@ -1,46 +1,75 @@
-let siteDiv = document.querySelector("#album");
+console.log("hello");
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+    // https://www.javascripttutorial.net/dom/manipulating/remove-all-child-nodes/
+  }
+}
 
-fetch("https://proxy-itunes-api.glitch.me/search?term=TOOL&media=music", {
-  method: "GET",
-  headers: {},
-})
-  .then(function (response) {
-    // console.log(response);
-    return response.json();
-  })
-  .then(function (data) {
-    // console.log(data);
+const siteDiv = document.querySelector("#album");
+// defining variable and connecting to html
+const form = document.querySelector("#search");
+const audioEl = document.querySelector("#previewMusic");
 
-    for (let i of data.results) {
-      console.log(i);
-      let bandDiv = document.createElement("div");
-      bandDiv.classList.add("band");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-      let artistDiv = document.createElement("div");
-      artistDiv.classList.add("artist");
-      artistDiv.innerText = i.artistName;
-      bandDiv.appendChild(artistDiv);
-      console.log(i.artistName);
+  // adding listener to click or enter
+  let inputBox = document.querySelector("#artistsearch");
+  console.log(inputBox.value);
 
-      let imageEl = document.createElement("img");
-      imageEl.src = i.artworkUrl100;
-      imageEl.classList.add("art");
-      bandDiv.appendChild(imageEl);
+  removeAllChildNodes(siteDiv);
 
-      let albumDiv = document.createElement("div");
-      albumDiv.classList.add("album");
-      albumDiv.innerText = i.collectionName;
-      bandDiv.appendChild(albumDiv);
-
-      let trackDiv = document.createElement("div");
-      trackDiv.classList.add("track");
-      bandDiv.appendChild(trackDiv);
-
-      let previewDiv = document.createElement("a");
-      previewDiv.href = `${i.trackViewUrl}`;
-      previewDiv.innerText = i.trackName;
-      bandDiv.appendChild(previewDiv);
-
-      siteDiv.appendChild(bandDiv);
+  fetch(
+    `https://proxy-itunes-api.glitch.me/search?term=${inputBox.value}&media=music`,
+    {
+      method: "GET",
+      headers: {},
     }
-  });
+  )
+    .then(function (response) {
+      // console.log(response);
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+
+      for (let i of data.results) {
+        console.log(i);
+        let bandDiv = document.createElement("div");
+        bandDiv.classList.add("band");
+
+        let artistDiv = document.createElement("div");
+        artistDiv.classList.add("artist");
+        artistDiv.innerText = i.artistName;
+        bandDiv.appendChild(artistDiv);
+        console.log(i.artistName);
+
+        let imageEl = document.createElement("img");
+        imageEl.src = i.artworkUrl100;
+        imageEl.classList.add("art");
+        bandDiv.appendChild(imageEl);
+
+        let albumDiv = document.createElement("div");
+        albumDiv.classList.add("album");
+        albumDiv.innerText = i.collectionName;
+        bandDiv.appendChild(albumDiv);
+
+        let trackDiv = document.createElement("div");
+        trackDiv.classList.add("track");
+        bandDiv.appendChild(trackDiv);
+
+        let previewDiv = document.createElement("a");
+        previewDiv.href = `${i.trackViewUrl}`;
+        previewDiv.innerText = i.trackName;
+        previewDiv.addEventListener("click", (event) => {
+          event.preventDefault();
+          audioEl.src = i.previewUrl;
+          audioEl.play();
+        });
+        bandDiv.appendChild(previewDiv);
+
+        siteDiv.appendChild(bandDiv);
+      }
+    });
+});
